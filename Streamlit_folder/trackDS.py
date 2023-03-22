28c81646ed143b6e95ce15322476116dc60bc251
Lucas Alberto <5lucas5alberto5@gmail.com>
2023-03-21 17:20:06 -0700

    created a interactable form for the ML model and wrote in calling the ML model from a diffrent folder

--- Streamlit_folder/trackDS.py
+++ Streamlit_folder/trackDS.py
@@ -6,12 +6,15 @@ import numpy as np
 import seaborn as sns
 import plotly.express as px
 import os
+import joblib
+import time
 
 
 #st.set_page_config(layout="wide")
 
-#os.chdir(r"C:\Users\5luca\Documents\Python\Projects\Track_DS\1merged_df")
-os.chdir("/Users/rommellp/Desktop/Track_DS_Project/Clean_visual_code/1merged_df/")
+os.chdir(r"C:\Users\5luca\Documents\Python\Projects\Track_DS\1merged_df")
+#os.chdir(r"C:\Users\5luca\Documents\Python\Projects\Track_DS\models")
+#os.chdir("/Users/rommellp/Desktop/Track_DS_Project/Clean_visual_code/1merged_df/")
 
 #calling in data and remocing unnecessary column
 Data48  = pd.read_csv("merged_400m_800m_df.csv", index_col = "ID Number")
@@ -136,21 +139,45 @@ with tab1:
                              size_max=150,)
             st.plotly_chart(fig, theme="streamlit", use_container_width=True)
             
-with tab2: 
-    
-    grade2 = st.selectbox("Select a grade level",['9th Grade Level', '10th Grade Level', '11th Grade Level', '12th Grade Level'], index=1)
-    #grade2
-    #p1 = sns.relplot(data=Data48, x='400 Meters', y='800 Meters', kind='scatter', hue='Season', palette ='deep', col="Grade Level")
-    #plt.subplots_adjust(wspace=0.2)
-    #p1.set_axis_labels("400 Meter Time(s)", "800 Meter Time(s)")
-    #p1.set(ylim=(100, None))
-    #p1.set(xlim=(45, None))
-
-    
-    
-    
-    
-    
-    
-    
-    
+with tab2:
+    final_model_reloaded = joblib.load("final_model_linreg.pkl") #calling the ML model
+    col3, col4 = st.columns(2)
+    with col3:
+            with st.form("my_form"): #creating a for for the user
+                st.write("Inside the form") #WRITE HERE
+                pastgrade=st.selectbox("Select your grade level",['9th Grade', '10th Grade', '11th Grade', '12th Grade'] ,index=0)
+                
+                #conditions for how each individual grade uses a unique array in the ML model
+                if(pastgrade == '9th Grade'):
+                        array1 = [1,0,0,0]
+                elif (pastgrade == '10th Grade'):
+                          array1 = [0,1,0,0]
+                elif (pastgrade == '11th Grade'):
+                          array1 = [0,0,1,0]
+                elif (pastgrade == '12th Grade'):
+                        array1 = [0,0,0,1]
+                        
+                #user will input thier time in seconds        
+                number = st.number_input('Enter your time:', format = '%f', help=None) 
+                array0 = np.array([number]) #putting float into an array 
+                array3 = np.concatenate((array0,array1), axis=0) #combining array 0 and array 1 to format data input for ML model
+                new_data = array3.reshape(1,-1) #correct array shape to horizantal            
+                predictions = final_model_reloaded.predict(new_data)#pushing data from the user into the ML model
+                predictions1 = predictions.astype(int)# i dont think this is needed anymore check on this again
+                
+                submitted = st.form_submit_button("Submit") #submit button 
+                
+                if submitted:
+                    progress = st.progress(0) #the start of progress bar = 0
+                    for i in range (100):
+                        time.sleep(0.05)# modify number if you want to change how long the progress takes, lower the number the faster 
+                        progress.progress(i+1)
+                        
+                    st.write("Estimated time for 800m:", predictions1[0], "seconds") #prints out results
+                    
+                  
+                
+                
+                
+                
+                
