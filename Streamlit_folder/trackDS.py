c4bd27f3d51d563af75caf28d0512ea4eea0e5bb
Lucas Alberto <5lucas5alberto5@gmail.com>
2023-03-27 12:52:04 -0700

    added a filter for dataframe, and customization options for the app itself

--- Streamlit_folder/trackDS.py
+++ Streamlit_folder/trackDS.py
@@ -11,22 +11,44 @@ import time
 import datetime
 import base64
 from PIL import Image
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,)
+
+st.set_page_config(layout="wide", page_title="Track DS")
 
 os.chdir(r"C:\Users\5luca\Documents\Python\Projects\Track_DS\1merged_df")
 #os.chdir(r"C:\Users\5luca\Documents\Python\Projects\Track_DS\models")
 #os.chdir("/Users/rommellp/Desktop/Track_DS_Project/Clean_visual_code/1merged_df/")
 #os.chdir("/Users/rommellp/Desktop/Track_DS_Project/Clean_visual_code/models/Modeling_400_800_final.ipynb")
 
-# Loading Image using PIL
-#im = Image.open('TrackDS_icon.jpg')
-# Adding Image to web app
-st.set_page_config(page_title="Track DS")
-#st.set_page_config(layout="wide")
+#This is what gets an image for the background
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+#set_background('track.png') #here goes the image name, since we are going to 1merged_df, the image must be in that folder
 
 
-
-
-#calling in data and remocing unnecessary column
+#calling in data and removing unnecessary column
 Data48  = pd.read_csv("merged_400m_800m_df.csv", index_col = "ID Number")
 Data48  = Data48.drop(columns=['Unnamed: 0'])
 
@@ -59,37 +81,97 @@ tab1, tab2 = st.tabs(["Data Visualization","Something else you might think of"])
 with tab1:
     
     #Create two columns/filters
-    col1, col2 = st.columns(2)
+    col1, col2= st.columns(2)
     with col1:
     
         racetype=st.selectbox("View by race type",['400m to 800m', '400m to 1500m','400m to 1600m','800m to 1500m','800m to 1600m'],index=0)
          
         if (racetype == '400m to 800m'): 
-            df = Data48
+            df1 = Data48
         elif (racetype == '400m to 1500m'): 
-            df = Data415
+            df1 = Data415
         elif (racetype == '400m to 1600m'): 
-            df = Data416
+            df1 = Data416
         elif (racetype == '800m to 1500m'): 
-            df = Data815
+            df1 = Data815
         elif (racetype == '800m to 1600m'): 
-            df = Data816
+            df1 = Data816
 
     with col2:
         
         grade = st.selectbox("Select a grade level", ['9th Grade', '10th Grade', '11th Grade', '12th Grade'], index=0)
-        racdf = df.loc[df['Grade Level']== grade]
+        df = df1.loc[df1['Grade Level']== grade]
         
-    with st.expander("Click to view.....", expanded=True):
-        st.write("Here are more details demonstrating.....")          
-        st.write(racdf)
-    with st.expander("Click to view....."):
-        st.write("Here are more details demonstrating.....")
-        #col1, col2 = st.columns(2)
-        #with col1:
-        
-        
- 
+    col3, col4 = st.columns(2)
+            
+    with col3: #Filter code
+        def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+                    modify = st.checkbox("Add filters")
+
+                    if not modify:
+                        return df
+
+                    df = df.copy()
+
+                    for col in df.columns:
+                        if is_object_dtype(df[col]):
+                            try:
+                                df[col] = pd.to_datetime(df[col])
+                            except Exception:
+                                pass
+
+                        if is_datetime64_any_dtype(df[col]):
+                            df[col] = df[col].dt.tz_localize(None)
+
+                    modification_container = st.container() 
+
+                    with modification_container:
+                        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+                        for column in to_filter_columns:
+                            left, right = st.columns((1, 20))
+                            # Treat columns with < 10 unique values as categorical
+                            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                                user_cat_input = right.multiselect(
+                                    f"Values for {column}",
+                                    df[column].unique(),
+                                    default=list(df[column].unique()),
+                                )
+                                df = df[df[column].isin(user_cat_input)]
+                            elif is_numeric_dtype(df[column]):
+                                _min = float(df[column].min())
+                                _max = float(df[column].max())
+                                step = (_max - _min) / 100
+                                user_num_input = right.slider(
+                                    f"Values for {column}",
+                                    min_value=_min,
+                                    max_value=_max,
+                                    value=(_min, _max),
+                                    step=step,
+                                )
+                                df = df[df[column].between(*user_num_input)]
+                            elif is_datetime64_any_dtype(df[column]):
+                                user_date_input = right.date_input(
+                                    f"Values for {column}",
+                                    value=(
+                                        df[column].min(),
+                                        df[column].max(),
+                                    ),
+                                )
+                                if len(user_date_input) == 2:
+                                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                                    start_date, end_date = user_date_input
+                                    df = df.loc[df[column].between(start_date, end_date)]
+                            else:
+                                user_text_input = right.text_input(
+                                    f"Substring or regex in {column}",
+                                )
+                                if user_text_input:
+                                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+                    return df       
+        st.dataframe(filter_dataframe(df)) #print data with filter
+
+    with col4:   
         if (racetype == '400m to 800m'): 
             df = Data48
             grade_type = df.loc[df['Grade Level']== grade]
@@ -150,12 +232,8 @@ with tab1:
                              #color_continuous_scale="")
                              size_max=150,)
             st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-            
+
 with tab2:
-    #os.chdir("/Users/rommellp/Desktop/Track_DS_Project/Clean_visual_code/models/")
-    
-    
-    
     st.markdown("The prediction below is for predicting an 800m time based on their 400m time and grade level. This example is one of the many pairs of models. There is a model for every \
             event pair seen on the charts on the data visualization tab.")
     
