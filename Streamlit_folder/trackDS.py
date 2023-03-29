05b814c594114672ed5e472f41768a00408ab0b2
Lucas Alberto <5lucas5alberto5@gmail.com>
2023-03-29 10:03:33 -0700

    filter now works with the scatterplots and cleaned code

--- Streamlit_folder/trackDS.py
+++ Streamlit_folder/trackDS.py
@@ -17,15 +17,14 @@ from pandas.api.types import (
     is_numeric_dtype,
     is_object_dtype,)
 
-st.set_page_config(layout="wide", page_title="Track DS")
+st.set_page_config(layout="wide", page_title="Track DS") #sets the page to a wide layout and adds title to the app 
 
-#os.chdir(r"C:\Users\5luca\Documents\Python\Projects\Track_DS\1merged_df")
+os.chdir(r"C:\Users\5luca\Documents\Python\Projects\Track_DS\1merged_df")
 #os.chdir(r"C:\Users\5luca\Documents\Python\Projects\Track_DS\models")
-os.chdir("/Users/rommellp/Desktop/Track_DS_Project/Clean_visual_code/1merged_df/")
+#os.chdir("/Users/rommellp/Desktop/Track_DS_Project/Clean_visual_code/1merged_df/")
 #os.chdir("/Users/rommellp/Desktop/Track_DS_Project/Clean_visual_code/models/Modeling_400_800_final.ipynb")
 
 #This is what gets an image for the background
-
 def get_base64(bin_file):
     with open(bin_file, 'rb') as f:
         data = f.read()
@@ -44,7 +43,6 @@ def set_background(png_file):
     
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
-
 #set_background('track.png') #here goes the image name, since we are going to 1merged_df, the image must be in that folder
 
 
@@ -75,10 +73,7 @@ st.sidebar.markdown("Welcome to my first app. This app is built using Streamlit 
 
 #Add title and subtitle to the main interface of the app
 st.title("Analyzing the Correlation Between 400m and 800m Runners")
-st.markdown("On the Data Visualization tab we have datafames where you can filter the event pairs and grade level. Below the chart is a scatter \
-            plot that uses the same filer. This plot also colors each point by season to further visualize the trend over time. On the right tab we have \
-            the machine learning model that predicts your 800m and 1500m time based on the input of your 400m / 800m time respectively, with \
-            grade level.")
+st.markdown("On the Data Visualization tab we have datafames where you can filter the event pairs and grade level. Below the chart is a scatter plot that uses the same filer. This plot also colors each point by season to further visualize the trend over time. On the right tab we have the machine learning model that predicts your 800m and 1500m time based on the input of your 400m and 800m time respectively, with grade level.")
 
 # add two tabs for different purposes
 tab1, tab2 = st.tabs(["Data Visualization","Prediction Modeling"])
@@ -89,9 +84,9 @@ with tab1:
     #Create two columns/filters
     col1, col2= st.columns(2)
     with col1:
-    
+        #select box for the user
         racetype=st.selectbox("View by race type",['400m to 800m', '400m to 1500m','400m to 1600m','800m to 1500m','800m to 1600m'],index=0)
-         
+        # if statements that chooses between the 5 different data sets
         if (racetype == '400m to 800m'): 
             df1 = Data48
         elif (racetype == '400m to 1500m'): 
@@ -104,10 +99,10 @@ with tab1:
             df1 = Data816
 
     with col2:
+        grade = st.selectbox("Select a grade level", ['9th Grade', '10th Grade', '11th Grade', '12th Grade'], index=0) #grade selection
+        df = df1.loc[df1['Grade Level']== grade] #filters grade types based on users selection
         
-        grade = st.selectbox("Select a grade level", ['9th Grade', '10th Grade', '11th Grade', '12th Grade'], index=0)
-        df = df1.loc[df1['Grade Level']== grade]
-        
+    #creating two more columns underneath col1 and col2    
     col3, col4 = st.columns(2)
             
     with col3: #Filter code
@@ -174,14 +169,13 @@ with tab1:
                                 if user_text_input:
                                     df = df[df[column].astype(str).str.contains(user_text_input)]
 
-                    return df       
-        st.dataframe(filter_dataframe(df)) #print data with filter
+                    return df
+        cleandata = filter_dataframe(df) #naming the filtered data to use for dataframe and scatter plots      
+        st.dataframe(cleandata) #print data with filter
 
-    with col4:   
-        if (racetype == '400m to 800m'): 
-            df = Data48
-            grade_type = df.loc[df['Grade Level']== grade]
-            fig = px.scatter( grade_type, 
+    with col4: #these if statemts determine the label axis of the graph, still using "cleandata"   
+        if (racetype == '400m to 800m'):
+            fig = px.scatter( cleandata, 
                              x = "400 Meters", 
                              y = "800 Meters", 
                              color = "Season", 
@@ -190,10 +184,8 @@ with tab1:
                              size_max=150,)
             st.plotly_chart(fig, theme="streamlit", use_container_width=True)
             
-        elif (racetype == '400m to 1500m'): 
-            df = Data415
-            grade_type = df.loc[df['Grade Level']== grade]
-            fig = px.scatter( grade_type, 
+        elif (racetype == '400m to 1500m'):
+            fig = px.scatter( cleandata, 
                              x = "400 Meters", 
                              y = "1500 Meters", 
                              color = "Season", 
@@ -201,12 +193,9 @@ with tab1:
                              #color_continuous_scale="")
                              size_max=150,)
             st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-          
-            
-        elif (racetype == '400m to 1600m'): 
-            df = Data416
-            grade_type = df.loc[df['Grade Level']== grade]
-            fig = px.scatter( grade_type, 
+               
+        elif (racetype == '400m to 1600m'):
+            fig = px.scatter( cleandata, 
                              x = "400 Meters", 
                              y = "1600 Meters", 
                              color = "Season", 
@@ -215,10 +204,8 @@ with tab1:
                              size_max=150,)
             st.plotly_chart(fig, theme="streamlit", use_container_width=True)
             
-        elif (racetype == '800m to 1500m'): 
-            df = Data815
-            grade_type = df.loc[df['Grade Level']== grade]
-            fig = px.scatter( grade_type, 
+        elif (racetype == '800m to 1500m'):
+            fig = px.scatter( cleandata, 
                              x = "800 Meters", 
                              y = "1500 Meters", 
                              color = "Season", 
@@ -227,10 +214,8 @@ with tab1:
                              size_max=150,)
             st.plotly_chart(fig, theme="streamlit", use_container_width=True)
             
-        elif (racetype == '800m to 1600m'): 
-            df = Data816
-            grade_type = df.loc[df['Grade Level']== grade]
-            fig = px.scatter( grade_type, 
+        elif (racetype == '800m to 1600m'):
+            fig = px.scatter( cleandata, 
                              x = "800 Meters", 
                              y = "1600 Meters", 
                              color = "Season", 
@@ -240,24 +225,19 @@ with tab1:
             st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 with tab2:
-    st.markdown("The prediction below is for predicting an 800m time based on their 400m time and grade level. This example is one of the many pairs of models. There is a model for every \
-            event pair seen on the charts on the data visualization tab.")
+    st.markdown("The prediction below is for predicting an 800m time based on their 400m time and grade level. This example is one of the many pairs of models. There is a model for every event pair seen on the charts on the data visualization tab.")
     
-    st.markdown("The model used to predict is a linear regression model. As seen on the plot on the visualization tab, each combination of events and grade level shows a clear linear \
-            correlation. Although many different regression models were tried, the simply linear regression model produced the best results. The results of this model were a RMSE of 8.7, and \
-            an accuracy score of 59%. ")
+    st.markdown("The model used to predict is a linear regression model. As seen on the plot on the visualization tab, each combination of events and grade level shows a clear linear correlation. Although many different regression models were tried, the simply linear regression model produced the best results. The results of this model were a RMSE of 8.7, and an accuracy score of 59%. ")
 
-    st.markdown("The model used for 800/1500m is the RandomForestRegressor with optimized parameters. The data used for this model can be viewed in the visualization tab, just as with \
-            the previous model. The results for this model were an RMSE of 14.8 and an accuracy score of 75%. I consider this a good result for this model in the context of the limitations \
-            mentioned with the first model. ")
+    st.markdown("The model used for 800/1500m is the RandomForestRegressor with optimized parameters. The data used for this model can be viewed in the visualization tab, just as with the previous model. The results for this model were an RMSE of 14.8 and an accuracy score of 75%. I consider this a good result for this model in the context of the limitations mentioned with the first model. ")
     
     st.markdown(":red[Try out the two models above and let us know how accurate the predictions are for you!]")
     
-    col3, col4 = st.columns(2)
-    with col3:
+    col5, col6 = st.columns(2)
+    with col5:
             
             with st.form("my_form"): #creating a form for the user
-                st.write("ML prediction model for 400m and 800m") #WRITE HERE
+                st.write("ML prediction model for 400m and 800m") #Title for the form
                 pastgrade=st.selectbox("Select a grade level:",['9th Grade', '10th Grade', '11th Grade', '12th Grade'] ,index=0)
                 
                 final_model_reloaded = joblib.load("final_model_linreg.pkl") #calling the ML model
@@ -291,20 +271,16 @@ with tab2:
                     seconds = predictions1[0]
 
                     minutes, seconds = divmod(seconds, 60)
-                    #hours, minutes = divmod(minutes, 60)
                     st.write("Estimated time for 800m is:")
                     st.write("%02d:%02d" % (minutes, seconds))
-                    
-                    #st.write("Estimated time for 800m:",minutes, "seconds") #prints out results
-
-    with col4: 
+    with col6:
         
-            with st.form ("Second Model"):
-                st.write("ML Prediction Model for 800m and 1500m")
+        with st.form ("Second Model"):
+                st.write("ML Prediction Model for 800m and 1500m") #Title for the form
                 mse_rft = joblib.load("mse_rft.pkl") #calling the ML model
                 
                 gradeslc = st.selectbox("Select your grade level",['9th Grade', '10th Grade', '11th Grade', '12th Grade'] ,index=0)
-                
+                #conditions for how each individual grade uses a unique array in the ML model
                 if (gradeslc == '9th Grade'):
                     arrayin = [1,0,0,0]
                 elif (gradeslc == '10th Grade'):
@@ -333,13 +309,9 @@ with tab2:
                         seconds = predictions1[0]
 
                     minutes, seconds = divmod(seconds, 60)
-                    #hours, minutes = divmod(minutes, 60)
                     st.write("Estimated time for 800m in the next grade is:")
                     st.write("%02d:%02d" % (minutes, seconds))
-                    #st.write("Estimated time for 800m:", predictions1[0], "seconds") #prints out results
-
-    st.markdown("Now that you've had a chance to use the models above, give a thought about how accurate it is for you. If you noticed that \
-    the 800/1500 model was more accurate, that could be because the dataset used for that model is almost twice as big! That size different \
-    likely had a huge part to play with the substantial difference in accuracy scores of both models.\
-    Thank you to Athletic.net for collecting this data. It was a dream to be able to work on this project and visualize trends I've heard for \
-    ages in Track and field, but never tested.")
+                    
+    st.markdown("Now that you've had a chance to use the models above, give a thought about how accurate it is for you. If you noticed that the 800/1500 model was more accurate, that could be because the dataset used for that model is almost twice as big! That size different likely had a huge part to play with the substantial difference in accuracy scores of both models. Thank you to Athletic.net for collecting this data. It was a dream to be able to work on this project and visualize trends I've heard for ages in Track and field, but never tested.")
+        
+            
