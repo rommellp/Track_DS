1a69fbcb2bbe6d768cb8ef70213923149c703640
rommellp <rompenn1@icloud.com>
2023-03-30 18:04:03 -0700

    path

--- Streamlit_folder/trackDS.py
+++ Streamlit_folder/trackDS.py
@@ -226,7 +226,7 @@ with tab2:
                 st.write("ML prediction model for 400m and 800m") #Title for the form
                 pastgrade=st.selectbox("Select a grade level:",['9th Grade', '10th Grade', '11th Grade', '12th Grade'] ,index=0)
                 
-                final_model_reloaded = joblib.load("merged_df/final_model_linreg.pkl") #calling the ML model
+                final_model_reloaded = joblib.load("Streamlit_folder/merged_df/final_model_linreg.pkl") #calling the ML model
                 
                 #conditions for how each individual grade uses a unique array in the ML model
                 if(pastgrade == '9th Grade'):
@@ -263,7 +263,7 @@ with tab2:
         
         with st.form ("Second Model"):
                 st.write("ML Prediction Model for 800m and 1500m") #Title for the form
-                mse_rft = joblib.load("merged_df/mse_rft.pkl") #calling the ML model
+                mse_rft = joblib.load("Streamlit_folder/merged_df/mse_rft.pkl") #calling the ML model
                 
                 gradeslc = st.selectbox("Select your grade level",['9th Grade', '10th Grade', '11th Grade', '12th Grade'] ,index=0)
                 #conditions for how each individual grade uses a unique array in the ML model
